a9090ca17f0c835c1b558ce131f6edc449fdcb60
subhanjee <subanjee14@gmail.com>
2024-11-13 13:23:50 +0500

    donetos

--- app/components/about/index.js
+++ app/components/about/index.js
@@ -15,8 +15,8 @@ function About() {
   return (
     <div className="main-about">
       <div>
-        <p className="text-center font-semibold text-2xl">About</p>
-        <p className="text-center font-semibold text-4xl">We Meme Business!</p>
+        <h3 className="text-center font-medium text-2xl text-black">About</h3>
+        <h2 className="text-center font-semibold text-4xl text-black ">We Meme Business!</h2>
         <div className="bg-about">
           <div className="grid grid-cols-1  md:grid-cols-3">
             <div className="flex justify-center items-center">
@@ -31,10 +31,10 @@ function About() {
               <Image src={qout1} alt="" className="" />
             </div>
           </div>
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center relative z-50">
           <div className="p-[2px] rounded-3xl bg-gradient-to-b from-orange-300 to-purple-300">
           <div className="py-5 px-5 md:py-8 md:px-12 bg-white rounded-3xl ">
-              <h1 className="text-center font-bold text-4xl text-black">Roadmap</h1>
+              <h1 className="text-center font-medium text-4xl text-black">Roadmap</h1>
               <div className="div-pos-road text-black">
                 <h2 className="text-center font-bold text-4xl mb-5">Q4 2024</h2>
                 <p className="text-center  text-1xl  ">
@@ -47,15 +47,15 @@ function About() {
                 <p className="text-center  text-1xl">
                 $JOKE will create 1 Million<br/> Jokes and Memes.
                 </p>
-                {/* <Image src={random2} alt="" className="pos-vec-road1" /> */}
+                <Image src={random2} alt="" className=" pos-vec-road1" />
               </div>
               <div className="div-pos-road2 text-black">
               <h2 className="text-center font-bold text-4xl mb-5">Q2 2025</h2>
                 <p className="text-center  text-1xl">
                 Build the AI Joke Generator<br/> for social media.
                 </p>
-                {/* <Image src={random1} alt="" className="pos-vec-road" /> */}
-                {/* <Image src={random3} alt="" className="pos-vec-road2" /> */}
+                <Image src={random1} alt="" className="pos-vec-road" />
+                <Image src={random3} alt="" className="pos-vec-road2" />
                 {/* <Image src={random6} alt="" className="pos-vec-roadmbl1" /> */}
               </div>
               <div className="div-pos-road3 text-black">
@@ -63,7 +63,7 @@ function About() {
                 <p className="text-center  text-1xl">
                 Bringing 1 Billion+ users to<br/> JokeVerse.
                 </p>
-                {/* <Image src={random4} alt="" className="pos-vec-road3" /> */}
+                <Image src={random4} alt="" className="pos-vec-road3" />
               </div>
               <Image src={random} alt="" className="w-96 "  />
             </div>
