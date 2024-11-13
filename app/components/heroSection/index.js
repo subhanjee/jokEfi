ef599d1aa1d679352b3fb44bfe4731e80e05ec13
subhanjee <subanjee14@gmail.com>
2024-11-13 17:46:37 +0500

    doneheaders

--- app/components/heroSection/index.js
+++ app/components/heroSection/index.js
@@ -1,14 +1,24 @@
- import React from 'react'
- 
- function HeroSection() {
-   return (
-     <div>
-       <h1>
-        HeroSection
-       </h1>
-     </div>
-   )
- }
- 
- export default HeroSection
- 
+import React from "react";
+import joke from "../../../public/images/joke.png";
+import joke1 from "../../../public/images/Rectangle 24.png";
+import joke2 from "../../../public/images/Rectangle 25.png";
+import joker from "../../../public/images/image 8.png";
+import Image from "next/image";
+const JokeMemeUI = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center   p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div>
+          <Image src={joke} alt="abc" width={350} height={350} />
+          <div className="flex gap-1">
+          <Image className="mt-4 w-32 h-20" src={joke2} alt="abc" />
+          <Image className="mt-4 w-58 h-20" src={joke1} alt="abc"  />
+          </div>
+        </div>
+        <Image src={joker} alt="abc" width={300} height={300} />
+      </div>
+    </div>
+  );
+};
+
+export default JokeMemeUI;
