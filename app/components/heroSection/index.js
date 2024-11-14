eac7adf3aaa8186f6a4b9c16c0fa6879b394dc68
subhanjee <subanjee14@gmail.com>
2024-11-15 01:30:07 +0500

    donefinal

--- app/components/heroSection/index.js
+++ app/components/heroSection/index.js
@@ -23,15 +23,13 @@ import joker19 from "../../../public/images/Ellipse 9.png";
 import joker20 from "../../../public/images/Ellipse 7.png";
 import joker21 from "../../../public/images/Ellipse 1.png";
 
-
 import Image from "next/image";
-import "./style.css"
+import "./style.css";
 const HeroSection = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center   p-4 ">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10  ">
-      <div className=" ">
-
+        <div className=" ">
           <Image src={joker8} alt="abc" className="back-joke5" />
           <Image src={joker7} alt="abc" className="back-joke4 z-50" />
           <Image src={joker16} alt="abc" className="back-jokes1" />
@@ -44,21 +42,41 @@ const HeroSection = () => {
           <Image src={joker13} alt="abc" className="back-joke10" />
           <Image src={joker14} alt="abc" className="back-joke11" />
           <Image src={joker21} alt="abc" className="back-jokes6" />
-          <Image src={joke} alt="abc" width={350} height={350} className="relative z-50"/>
+          <Image
+            src={joke}
+            alt="abc"
+            width={350}
+            height={350}
+            className="relative z-50"
+          />
           <div className="flex gap-1">
-          <Image className="mt-4 w-32 h-20 relative z-50" src={joke2} alt="abc" />
-          <Image className="mt-4 w-58 h-20 relative z-50" src={joke1} alt="abc"  />
+            <Image
+              className="mt-4 w-32 h-20 relative z-50"
+              src={joke2}
+              alt="abc"
+            />
+            <Image
+              className="mt-4 w-58 h-20 relative z-50"
+              src={joke1}
+              alt="abc"
+            />
           </div>
         </div>
         <div className="flex justify-center items-center">
-        <Image src={joker} alt="abc" width={250} height={250} className="-mt-5 lg:-mt-10 relative z-50"/>
-        <Image src={joker4} alt="abc" className="back-joke1" />
-        <Image src={joker5} alt="abc" className="back-joke2" />
-        <Image src={joker9} alt="abc" className="back-joke6" />
-        <Image src={joker18} alt="abc" className="back-jokes3" />
-        <Image src={joker19} alt="abc" className="back-jokes4" />
-        <Image src={joker10} alt="abc" className="back-joke7" />
-        <Image src={joker20} alt="abc" className="back-jokes5" />
+          <Image
+            src={joker}
+            alt="abc"
+            width={250}
+            height={250}
+            className="-mt-5 lg:-mt-10 relative z-50"
+          />
+          <Image src={joker4} alt="abc" className="back-joke1" />
+          <Image src={joker5} alt="abc" className="back-joke2" />
+          <Image src={joker9} alt="abc" className="back-joke6" />
+          <Image src={joker18} alt="abc" className="back-jokes3" />
+          <Image src={joker19} alt="abc" className="back-jokes4" />
+          <Image src={joker10} alt="abc" className="back-joke7" />
+          <Image src={joker20} alt="abc" className="back-jokes5" />
         </div>
       </div>
     </div>
