f855ffe6ffeaff05cdafc5dda4eccd073c34bf63
subhanjee <subanjee14@gmail.com>
2024-11-15 02:20:10 +0500

    donefinal

--- app/components/animation/index.js
+++ app/components/animation/index.js
@@ -1,22 +1,26 @@
  "use client";
 import React from "react";
+import cloud from "../../../public/images/Yuppies Doodle Cloud 2.png";
+import Image from "next/image";
 
 function Animation() {
   return (
-    <div className="py-3 z-50">
-    <div className="overflow-hidden bg-black text-white text-center p-3 shadow-2xl whitespace-nowrap relative z-50">
+    <div className=" py-5 z-50">
+            <Image src={cloud} alt="abc" className="bac-immm" />
+
+    <div className="overflow-hidden bg-black text-white text-center p-4 shadow-2xl whitespace-nowrap relative z-50">
       <div className="flex animate-scroll space-x-4 text-sm">
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
         {/* Duplicate content for seamless scroll */}
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</span>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit |</h3>
       </div>
     </div>
     </div>
--- app/components/header/index.js
+++ app/components/header/index.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import telegram from "../../../public/images/telegram.png"
+import telegram from "../../../public/images/telegram.png";
 import Image from "next/image";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,49 +11,50 @@ const Header = () => {
   };
 
   return (
-    <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-2xl relative z-50">
+    <nav className="flex items-center justify-between  px-6 py-4 bg-white shadow-2xl relative z-50">
       <div className="text-lg font-semibold text-black"> </div>
-
       {/* Desktop Menu */}
       <div className="hidden md:flex space-x-8">
         <Link href="/" className="text-black  hover:text-gray-700">
-          <h4 className="font-bold text-md lg:text-2xl">Home</h4>
+          <h4 className="font-bold text-md lg:text-2xl xl:text-3xl">Home</h4>
         </Link>
         <span className="text-black font-semibold">|</span>
         <Link
           href="#about"
           className="text-black font-semibold hover:text-gray-700"
         >
-          <h4 className="font-bold text-md lg:text-2xl">About</h4>
+          <h4 className="font-bold text-md lg:text-2xl xl:text-3xl">About</h4>
         </Link>
         <span className="text-black font-semibold">|</span>
         <Link
           href="#roadmap"
           className="text-black font-semibold hover:text-gray-700"
         >
-          <h4 className="font-bold text-md lg:text-2xl">Roadmap</h4>
+          <h4 className="font-bold text-md lg:text-2xl xl:text-3xl">Roadmap</h4>
         </Link>
         <span className="text-black font-semibold">|</span>
         <Link
           href="#tokenomics"
           className="text-black font-semibold hover:text-gray-700"
         >
-          <h4 className="font-bold text-md lg:text-2xl">Tokenomics</h4>
+          <h4 className="font-bold text-md lg:text-2xl xl:text-3xl">
+            Tokenomics
+          </h4>
         </Link>
         <span className="text-black font-semibold">|</span>
         <Link
           href="#contact"
           className="text-black font-semibold hover:text-gray-700"
         >
-          <h4 className="font-bold text-md lg:text-2xl">Contact</h4>
+          <h4 className="font-bold text-md lg:text-2xl xl:text-3xl">Contact</h4>
         </Link>
       </div>
 
       {/* Telegram Button for Desktop */}
       <div className="hidden md:flex">
         <button className="flex gap-5 items-center px-6 lg:px-10 py-2 shadow-xl text-white bg-black rounded-md hover:bg-gray-800 relative z-50">
-          <Image src={telegram} alt="abc" width={15} height={15}/>
-         <h4 className='font-bold text-md lg:text-2xl'>Telegram</h4> 
+          <Image src={telegram} alt="abc" width={15} height={15} />
+          <h4 className="font-bold text-md lg:text-2xl">Telegram</h4>
         </button>
       </div>
 
@@ -84,40 +85,40 @@ const Header = () => {
               onClick={toggleMenu}
               className="text-black font-semibold hover:text-gray-700"
             >
-              <h4 className='font-bold'>Home</h4>
+              <h4 className="font-bold">Home</h4>
             </Link>
             <Link
               href="#about"
               onClick={toggleMenu}
               className="text-black font-semibold hover:text-gray-700"
             >
-             <h4 className='font-bold'>About</h4> 
+              <h4 className="font-bold">About</h4>
             </Link>
             <Link
               href="#roadmap"
               onClick={toggleMenu}
               className="text-black font-semibold hover:text-gray-700"
             >
-              <h4 className='font-bold'>Roadmap</h4>
+              <h4 className="font-bold">Roadmap</h4>
             </Link>
             <Link
               href="#tokenomics"
               onClick={toggleMenu}
               className="text-black font-semibold hover:text-gray-700"
             >
-             <h4 className='font-bold'>Tokenomics</h4> 
+              <h4 className="font-bold">Tokenomics</h4>
             </Link>
             <Link
               href="#contact"
               onClick={toggleMenu}
               className="text-black font-semibold hover:text-gray-700"
             >
-             <h4 className='font-bold'>Contact</h4> 
+              <h4 className="font-bold">Contact</h4>
             </Link>
             <button className="flex gap-5 items-center px-10 py-2 shadow-xl text-white bg-black rounded-md hover:bg-gray-800">
-          <Image src={telegram} alt="abc" width={15} height={15}/>
-         <h4 className='font-bold'>Telegram</h4> 
-        </button>
+              <Image src={telegram} alt="abc" width={15} height={15} />
+              <h4 className="font-bold">Telegram</h4>
+            </button>
           </div>
         </div>
       )}
