9f69ce97d416a06bd6be554a9563bb6710a4d3f0
subhanjee <subanjee14@gmail.com>
2024-11-15 19:48:28 +0500

    donefinalmb

--- app/components/header/index.js
+++ app/components/header/index.js
@@ -2,6 +2,9 @@
 import { useState } from "react";
 import Link from "next/link";
 import telegram from "../../../public/images/telegram.png";
+import ava from "../../../public/images/Dexscreener (1).png";
+import x from "../../../public/images/X.png";
+import TG from "../../../public/images/TG.png";
 import Image from "next/image";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +14,7 @@ const Header = () => {
   };
 
   return (
-    <nav className="flex items-center justify-between  px-6 py-2 bg-white shadow-2xl relative z-50">
+    <nav className="flex items-center justify-between  px-6 py-2 bg-white shadow-2xl  relative z-50">
       <div className="text-lg font-semibold text-black"> </div>
       {/* Desktop Menu */}
       <div className="hidden md:flex space-x-8">
@@ -37,17 +40,21 @@ const Header = () => {
           href="#tokenomics"
           className="text-black font-semibold hover:text-gray-700"
         >
-          <h4 className="font-bold text-md lg:text-1xl ">
-            Tokenomics
-          </h4>
+          <h4 className="font-bold text-md lg:text-1xl ">Tokenomics</h4>
         </Link>
         <span className="text-black font-semibold">|</span>
-        <Link
-          href="#contact"
-          className="text-black font-semibold hover:text-gray-700"
-        >
-          <h4 className="font-bold text-md lg:text-1xl ">Contact</h4>
-        </Link>
+        <div className="flex gap-4">
+          <a href="https://dexscreener.com/" target="_blank">
+            <Image src={ava} alt="abc" className="w-6 h-6" />
+          </a>
+          <a href="https://twitter.com/?lang=en" target="_blank">
+            {" "}
+            <Image src={x} alt="abc" className="w-5 h-5" />
+          </a>
+          <a href="https://telegram.org/" target="_blank">
+            <Image src={TG} alt="abc" className="w-5 h-5" />
+          </a>
+        </div>
       </div>
 
       {/* Telegram Button for Desktop */}
@@ -108,13 +115,18 @@ const Header = () => {
             >
               <h4 className="font-bold">Tokenomics</h4>
             </Link>
-            <Link
-              href="#contact"
-              onClick={toggleMenu}
-              className="text-black font-semibold hover:text-gray-700"
-            >
-              <h4 className="font-bold">Contact</h4>
-            </Link>
+            <div className="flex gap-4">
+          <a href="https://dexscreener.com/" target="_blank">
+            <Image src={ava} alt="abc" className="w-6 h-6" />
+          </a>
+          <a href="https://twitter.com/?lang=en" target="_blank">
+            {" "}
+            <Image src={x} alt="abc" className="w-5 h-5" />
+          </a>
+          <a href="https://telegram.org/" target="_blank">
+            <Image src={TG} alt="abc" className="w-5 h-5" />
+          </a>
+        </div>
             <button className="flex gap-5 items-center px-10 py-2 shadow-xl text-white bg-black rounded-md hover:bg-gray-800">
               <Image src={telegram} alt="abc" width={15} height={15} />
               <h4 className="font-bold">Telegram</h4>
